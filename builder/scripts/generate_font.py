e514fdd32674be1db567a6874de07b42de077f57
AJ Dunn <Andrew.Dunn2@sa.gov.au>
2016-03-11 14:10:28 +1030

    Add new fonts, modify all old fonts and change build settings

--- builder/scripts/generate_font.py
+++ builder/scripts/generate_font.py
@@ -15,7 +15,7 @@ INPUT_SVG_DIR = os.path.join(SCRIPT_PATH, '..', '..', 'src')
 OUTPUT_FONT_DIR = os.path.join(SCRIPT_PATH, '..', '..', 'dist', 'fonts')
 MANIFEST_PATH = os.path.join(SCRIPT_PATH, '..', 'manifest.json')
 BUILD_DATA_PATH = os.path.join(SCRIPT_PATH, '..', 'build_data.json')
-AUTO_WIDTH = True
+AUTO_WIDTH = False
 KERNING = 15
 
 cp = 0xf100
@@ -23,10 +23,10 @@ m = md5.new()
 
 f = fontforge.font()
 f.encoding = 'UnicodeFull'
-f.design_size = 16
-f.em = 512
-f.ascent = 448
-f.descent = 64
+# f.design_size = 16
+# f.em = 512
+# f.ascent = 448
+# f.descent = 64
 
 manifest_file = open(MANIFEST_PATH, 'r')
 manifest_data = json.loads(manifest_file.read())
@@ -136,72 +136,56 @@ for dirname, dirnames, filenames in os.walk(INPUT_SVG_DIR):
       if tmpsvgfile:
         os.unlink(tmpsvgfile.name)
 
-      # set glyph size explicitly or automatically depending on autowidth
-      if AUTO_WIDTH:
-        glyph.left_side_bearing = glyph.right_side_bearing = 0
-        glyph.round()
-
-    # resize glyphs if autowidth is enabled
-    if AUTO_WIDTH:
-      f.autoWidth(0, 0, 512)
-
-  fontfile = '%s/dmicons' % (OUTPUT_FONT_DIR)
+fontfile = '%s/dmicons' % (OUTPUT_FONT_DIR)
 
 build_hash = m.hexdigest()
 
-if build_hash == manifest_data.get('build_hash'):
-  print "Source files unchanged, did not rebuild fonts"
+manifest_data['build_hash'] = build_hash
 
-else:
-  manifest_data['build_hash'] = build_hash
+f.fontname = font_name
+f.familyname = font_name
+f.fullname = font_name
 
-  f.fontname = font_name
-  f.familyname = font_name
-  f.fullname = font_name
+print 'Generate TTF Font File'
+f.generate(fontfile + '.ttf')
 
-  print 'Generate TTF Font File'
-  f.generate(fontfile + '.ttf')
+print 'Generate SVG Font File'
+f.generate(fontfile + '.svg')
 
-  print 'Generate SVG Font File'
-  f.generate(fontfile + '.svg')
+# Fix SVG header for webkit
+# from: https://github.com/fontello/font-builder/blob/master/bin/fontconvert.py
+svgfile = open(fontfile + '.svg', 'r+')
+svgtext = svgfile.read()
+svgfile.seek(0)
+svgfile.write(svgtext.replace('''<svg>''', '''<svg xmlns="http://www.w3.org/2000/svg">'''))
+svgfile.close()
 
-  # Fix SVG header for webkit
-  # from: https://github.com/fontello/font-builder/blob/master/bin/fontconvert.py
-  svgfile = open(fontfile + '.svg', 'r+')
-  svgtext = svgfile.read()
-  svgfile.seek(0)
-  svgfile.write(svgtext.replace('''<svg>''', '''<svg xmlns="http://www.w3.org/2000/svg">'''))
-  svgfile.close()
+scriptPath = os.path.dirname(os.path.realpath(__file__))
+try:
+  subprocess.Popen([scriptPath + '/sfnt2woff', fontfile + '.ttf'], stdout=subprocess.PIPE)
+except OSError:
+  subprocess.call(['sfnt2woff', fontfile + '.ttf'])
 
-  scriptPath = os.path.dirname(os.path.realpath(__file__))
-  try:
-    subprocess.Popen([scriptPath + '/sfnt2woff', fontfile + '.ttf'], stdout=subprocess.PIPE)
-  except OSError:
-    # If the local version of sfnt2woff fails (i.e., on Linux), try to use the
-    # global version. This allows us to avoid forcing OS X users to compile
-    # sfnt2woff from source, simplifying install.
-    subprocess.call(['sfnt2woff', fontfile + '.ttf'])
+# eotlitetool.py script to generate IE7-compatible .eot fonts
+print 'Generate EOT Font File'
+subprocess.call('python ' + scriptPath + '/eotlitetool.py ' + fontfile + '.ttf -o ' + fontfile + '.eot', shell=True)
+subprocess.call('mv ' + fontfile + '.eotlite ' + fontfile + '.eot', shell=True)
 
-  # eotlitetool.py script to generate IE7-compatible .eot fonts
-  print 'Generate EOT Font File'
-  subprocess.call('python ' + scriptPath + '/eotlitetool.py ' + fontfile + '.ttf -o ' + fontfile + '.eot', shell=True)
-  subprocess.call('mv ' + fontfile + '.eotlite ' + fontfile + '.eot', shell=True)
+print 'Hint TTF file'
+subprocess.call('ttfautohint -s -f -n ' + fontfile + '.ttf ' + fontfile + '-hinted.ttf > /dev/null 2>&1 && mv ' + fontfile + '-hinted.ttf ' + fontfile + '.ttf', shell=True)
 
-  print 'Hint TTF file'
-  subprocess.call('ttfautohint -s -f -n ' + fontfile + '.ttf ' + fontfile + '-hinted.ttf > /dev/null 2>&1 && mv ' + fontfile + '-hinted.ttf ' + fontfile + '.ttf', shell=True)
+print 'Generate WOFF2 Font File'
+subprocess.call('woff2_compress ' + fontfile + '.ttf', shell=True)
 
-  print 'Generate WOFF2 Font File'
-  subprocess.call('woff2_compress ' + fontfile + '.ttf', shell=True)
+manifest_data['icons'] = sorted(manifest_data['icons'], key=lambda k: k['name'])
+build_data['icons'] = sorted(build_data['icons'], key=lambda k: k['name'])
 
-  manifest_data['icons'] = sorted(manifest_data['icons'], key=lambda k: k['name'])
-  build_data['icons'] = sorted(build_data['icons'], key=lambda k: k['name'])
+print "Save Manifest, Icons: %s" % ( len(manifest_data['icons']) )
+f = open(MANIFEST_PATH, 'w')
+f.write( json.dumps(manifest_data, indent=2, separators=(',', ': ')) )
+f.close()
 
-  print "Save Manifest, Icons: %s" % ( len(manifest_data['icons']) )
-  f = open(MANIFEST_PATH, 'w')
-  f.write( json.dumps(manifest_data, indent=2, separators=(',', ': ')) )
-  f.close()
-
-  print "Save Build, Icons: %s" % ( len(build_data['icons']) )
-  f = open(BUILD_DATA_PATH, 'w')
-  f.write( json.dumps(build_data, indent=2, separators=(',', ': ')) )
-  f.close()
+print "Save Build, Icons: %s" % ( len(build_data['icons']) )
+f = open(BUILD_DATA_PATH, 'w')
+f.write( json.dumps(build_data, indent=2, separators=(',', ': ')) )
+f.close()
