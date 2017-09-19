bf4c7d7755c8f7eda3b396b6f9811262dcb384a7
Adam Bradley <adambradley25@gmail.com>
2017-09-19 11:42:20 -0500

    fix(collection): ensure collection has a copy of svg js files

--- scripts/svg-to-js.js
+++ scripts/svg-to-js.js
@@ -4,9 +4,14 @@ var jpml = require('@ionic/jpml');
 var fs = require('fs-extra');
 var path = require('path');
 
-const OUT_DIR = path.join(__dirname, '..', 'dist', 'ionicons', 'svg');
+const OUT_DIR = [
+  path.join(__dirname, '..', 'dist', 'ionicons', 'svg'),
+  path.join(__dirname, '..', 'dist', 'collection', 'icon', 'svg')
+];
 
-fs.emptyDirSync(OUT_DIR);
+OUT_DIR.forEach(outDir => {
+  fs.emptyDirSync(outDir);
+});
 
 jpml.generate({
   include: ['dist/svg/'],
