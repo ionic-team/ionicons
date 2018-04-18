89547f71d74984cc9fae8f16a5ee3c03b5857f11
Kevin Ports <kevin.ports@gmail.com>
2018-04-18 17:30:10 -0500

    Add svg source file zip archiving to build scripts

---
+++ scripts/build-zip.js
@@ -0,0 +1,21 @@
+var fs = require('fs-extra');
+var path = require('path');
+var archiver = require('archiver');
+
+var svgsDir = path.join(__dirname, '..', 'dist','svg');
+var distzipDir = path.join(__dirname, '..', 'dist');
+
+var archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+var output = fs.createWriteStream(distzipDir + '/svg.zip');
+output.on('close', function() {
+  console.log('Svg zip archive created.');
+});
+
+archive.pipe(output);
+archive.directory(svgsDir, false);
+archive.on('error', function(err){
+    throw err;
+});
+archive.finalize();
