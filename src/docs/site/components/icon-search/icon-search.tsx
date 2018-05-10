f599e72c93fa75cb6aefc19cf3638184593ab0eb
Kevin Ports <kevin.ports@gmail.com>
2018-05-10 11:55:07 -0500

    Custom search input clear button

--- src/docs/site/components/icon-search/icon-search.tsx
+++ src/docs/site/components/icon-search/icon-search.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, Listen, Event, EventEmitter } from '@stencil/core';
+import { Component, Prop, Listen, Event, EventEmitter, State, Watch } from '@stencil/core';
 
 @Component({
   tag: 'icon-search',
@@ -9,19 +9,48 @@ export class IconSearch {
   @Prop() size: string = 'small';
   @Prop() autofocus: string = 'none';
 
+  @State() showClearCtrl: boolean = false;
+
   @Event() hasSearched: EventEmitter;
 
+  @Watch('query')
+  watchQuery() {
+    this.showClearCtrl = (this.query.length > 0) ? true : false;
+  }
+
+
   @Listen('keyup')
-  @Listen('search')
   searchListener(ev: any) {
+    if (ev.keyCode === 27) {
+      this.handleClear();
+      return;
+    }
+
     const value = (ev.target as HTMLInputElement).value;
     this.hasSearched.emit(value);
+  }
+
+  handleClear() {
+    this.hasSearched.emit('');
   }
 
   render() {
     return (
     <div class={`search-input search-input--${this.size}`}>
-      <input type="search" placeholder="Search icons..." value={this.query} autofocus={this.autofocus === 'autofocus' ? 'autofocus' : ''} />
+
+      <input type="text"
+        placeholder="Search icons..."
+        value={this.query}
+        autofocus={this.autofocus === 'autofocus' ? 'autofocus' : ''}/>
+
+      <i class={{
+          'search-input__clear': true,
+          'search-input__clear--active': this.showClearCtrl,
+          'ion': true,
+          'ion-md-close': true
+        }}
+        onClick={this.handleClear.bind(this)}></i>
+
     </div>
   )}
 }
