29f812aaafe7b33c21dd89399e03ac16368aac53
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-08-23 17:36:31 +0200

    chore(): update to latest stencil

--- src/components/generated.d.ts
+++ src/components/generated.d.ts
@@ -9,101 +9,103 @@ import { JSXElements } from '@stencil/core';
 
 
 
-export namespace StencilComponents {
 
-  interface IonIcon {
-    /**
-    * Specifies the label to use for accessibility. Defaults to the icon name.
-    */
-    'ariaLabel': string;
-    /**
-    * The color to use for the background of the item.
-    */
-    'color': string;
-    /**
-    * A combination of both `name` and `src`. If a `src` url is detected it will set the `src` property. Otherwise it assumes it's a built-in named SVG and set the `name` property.
-    */
-    'icon': string;
-    /**
-    * Specifies which icon to use on `ios` mode.
-    */
-    'ios': string;
-    /**
-    * If enabled, ion-icon will be loaded lazily when it's visible in the viewport. Default, `true`.
-    */
-    'lazy': boolean;
-    /**
-    * Specifies which icon to use on `md` mode.
-    */
-    'md': string;
-    /**
-    * The mode determines which platform styles to use. Possible values are: `"ios"` or `"md"`.
-    */
-    'mode': 'ios' | 'md';
-    /**
-    * Specifies which icon to use from the built-in set of icons.
-    */
-    'name': string;
-    /**
-    * The size of the icon. Available options are: `"small"` and `"large"`.
-    */
-    'size': string;
-    /**
-    * Specifies the exact `src` of an SVG file to use.
-    */
-    'src': string;
-  }
-  interface IonIconAttributes extends JSXElements.HTMLAttributes {
-    /**
-    * Specifies the label to use for accessibility. Defaults to the icon name.
-    */
-    'ariaLabel'?: string;
-    /**
-    * The color to use for the background of the item.
-    */
-    'color'?: string;
-    /**
-    * A combination of both `name` and `src`. If a `src` url is detected it will set the `src` property. Otherwise it assumes it's a built-in named SVG and set the `name` property.
-    */
-    'icon'?: string;
-    /**
-    * Specifies which icon to use on `ios` mode.
-    */
-    'ios'?: string;
-    /**
-    * If enabled, ion-icon will be loaded lazily when it's visible in the viewport. Default, `true`.
-    */
-    'lazy'?: boolean;
-    /**
-    * Specifies which icon to use on `md` mode.
-    */
-    'md'?: string;
-    /**
-    * The mode determines which platform styles to use. Possible values are: `"ios"` or `"md"`.
-    */
-    'mode'?: 'ios' | 'md';
-    /**
-    * Specifies which icon to use from the built-in set of icons.
-    */
-    'name'?: string;
-    /**
-    * The size of the icon. Available options are: `"small"` and `"large"`.
-    */
-    'size'?: string;
-    /**
-    * Specifies the exact `src` of an SVG file to use.
-    */
-    'src'?: string;
-  }
+interface IonIcon {
+  /**
+  * Specifies the label to use for accessibility. Defaults to the icon name.
+  */
+  'ariaLabel': string;
+  /**
+  * The color to use for the background of the item.
+  */
+  'color': string;
+  /**
+  * A combination of both `name` and `src`. If a `src` url is detected it will set the `src` property. Otherwise it assumes it's a built-in named SVG and set the `name` property.
+  */
+  'icon': string;
+  /**
+  * Specifies which icon to use on `ios` mode.
+  */
+  'ios': string;
+  /**
+  * If enabled, ion-icon will be loaded lazily when it's visible in the viewport. Default, `true`.
+  */
+  'lazy': boolean;
+  /**
+  * Specifies which icon to use on `md` mode.
+  */
+  'md': string;
+  /**
+  * The mode determines which platform styles to use. Possible values are: `"ios"` or `"md"`.
+  */
+  'mode': 'ios' | 'md';
+  /**
+  * Specifies which icon to use from the built-in set of icons.
+  */
+  'name': string;
+  /**
+  * The size of the icon. Available options are: `"small"` and `"large"`.
+  */
+  'size': string;
+  /**
+  * Specifies the exact `src` of an SVG file to use.
+  */
+  'src': string;
+}
+interface IonIconAttributes extends JSXElements.HTMLAttributes {
+  /**
+  * Specifies the label to use for accessibility. Defaults to the icon name.
+  */
+  'ariaLabel'?: string;
+  /**
+  * The color to use for the background of the item.
+  */
+  'color'?: string;
+  /**
+  * A combination of both `name` and `src`. If a `src` url is detected it will set the `src` property. Otherwise it assumes it's a built-in named SVG and set the `name` property.
+  */
+  'icon'?: string;
+  /**
+  * Specifies which icon to use on `ios` mode.
+  */
+  'ios'?: string;
+  /**
+  * If enabled, ion-icon will be loaded lazily when it's visible in the viewport. Default, `true`.
+  */
+  'lazy'?: boolean;
+  /**
+  * Specifies which icon to use on `md` mode.
+  */
+  'md'?: string;
+  /**
+  * The mode determines which platform styles to use. Possible values are: `"ios"` or `"md"`.
+  */
+  'mode'?: 'ios' | 'md';
+  /**
+  * Specifies which icon to use from the built-in set of icons.
+  */
+  'name'?: string;
+  /**
+  * The size of the icon. Available options are: `"small"` and `"large"`.
+  */
+  'size'?: string;
+  /**
+  * Specifies the exact `src` of an SVG file to use.
+  */
+  'src'?: string;
+}
+
+export interface LocalElementInterfaces  {
+  'IonIcon': IonIcon;
 }
 
 export interface LocalIntrinsicElements {
-  'ion-icon': StencilComponents.IonIconAttributes;
+  'ion-icon': IonIconAttributes;
 }
 
 declare global {
 
-  interface HTMLIonIconElement extends StencilComponents.IonIcon, HTMLStencilElement {}
+  interface HTMLIonIconElement extends IonIcon, HTMLStencilElement {}
   var HTMLIonIconElement: {
     prototype: HTMLIonIconElement;
     new (): HTMLIonIconElement;
