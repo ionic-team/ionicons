73bc0a596a074055f7cd9fcc9e1186a111e38e7a
Manu Mtz.-Almeida <manu.mtza@gmail.com>
2018-10-14 10:59:39 -0500

    chore(): update website

--- docs/build/docssite/chunk-f5f83825.js
+++
@@ -1,2 +0,0 @@
-/*! Built with http://stenciljs.com */
-const{h:e}=window.DocsSite;var o="undefined"!=typeof global?global:"undefined"!=typeof self?self:"undefined"!=typeof window?window:{};export{o as a};
