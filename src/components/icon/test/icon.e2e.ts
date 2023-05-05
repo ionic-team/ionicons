41b509c3623a7aff72ac5e3deb0a5da9c9cf5c2a
Shawn Taylor <shawn@ionic.io>
2023-05-05 10:57:52 -0400

    Add snapshot for rtl

--- src/components/icon/test/icon.e2e.ts
+++ src/components/icon/test/icon.e2e.ts
@@ -4,10 +4,23 @@ import { test } from '@utils/test/playwright';
 test.describe('icon: basic', () => {
   test('should not have visual regressions', async ({ page }) => {
     await page.goto(`/`);
-    
+
     // Wait for all SVGs to be lazily loaded before taking screenshots
     await page.waitForLoadState('networkidle');
 
     expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`icon-diff.png`);
+  });
+
+  test('some icons should flip when rtl', async ({ page }) => {
+    await page.goto(`/`);
+
+    await page.evaluate(() => {
+      document.dir = 'rtl';
+    });
+
+    // Wait for all SVGs to be lazily loaded before taking screenshots
+    await page.waitForLoadState('networkidle');
+
+    expect(await page.screenshot({ fullPage: true })).toMatchSnapshot(`icon-rtl-diff.png`);
   });
 });
